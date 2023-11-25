83547b9df4015b6a42d3638b956d34b86ec640e3
Ramon Estol <hello@ramonestol.com>
2023-11-25 13:34:54 +0000

    ex 3 create

---
+++ master-programacion/#3-diferencia-de-dias.js
@@ -0,0 +1,8 @@
+/*Enunciado Ejercicio 3:
+Dadas dos fechas, crea una función que me proporcione la diferencia de dias
+entre ellas
+ 
+Ejemplo:
+diferenciaDeDias('Dec 1, 2023', 'Dec 24, 2023');  // Salida: 23
+ 
+*/
