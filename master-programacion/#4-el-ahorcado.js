b7346f0cf0fc03ba6291103e786dfa74971704cb
Ramon Estol <hello@ramonestol.com>
2023-11-26 10:18:59 +0000

    solution ex4 ahorcado

---
+++ master-programacion/#4-el-ahorcado.js
@@ -0,0 +1,63 @@
+
+/* 
+Enunciado Ejercicio 4:
+Crea el juego del ahorcado.
+El usuario tendrá que ir adivinando y descubriendo las letras de una palabra 
+secreta, y tendrá 5 intentos (un intento por extremidad del cuerpo humano).
+ 
+Utiliza el método de entrada de datos habitual de tu lenguaje,
+en el caso de JS, usaremos la función prompt.
+ 
+Ejemplo:
+juegoDelAhorcado('victor');  
+ 
+// Salida:
+Adivina la letra: i
+La palabra es: _i___
+Te quedan 5 intentos
+ 
+
+*/
+const prompt = require("prompt-sync")();
+
+function hangmanGame(secretWord) {
+  //convertir la palabra en un array
+  const wordArr = secretWord.split("");
+  let resultArr = wordArr.map((e) => "_");
+  let cont = 5;
+
+  //crear un loop
+  do {
+    //pedir una letra
+    let userInput = prompt("Adivina la letra:");
+
+    //comprobar si existe y generar resultado
+    index = wordArr.indexOf(userInput);
+    let isFound=false;
+
+      while(index!== -1){
+        resultArr[index]= userInput;
+        index= wordArr.indexOf(userInput, index+1);
+        isFound= true;
+      }
+    
+    if(isFound===false) cont--;
+    
+  //comprobar si la palabra ya esta completa
+    if(wordArr.toString()===resultArr.toString()){
+      return `Felicidades has completado el ahorcado, la palabra secreta era "${secretWord}"`;
+    } 
+    else{
+      //mostrar palabra e intentos restantes
+      console.log(`
+      La palabra es: ${resultArr.join("")}
+      Te quedan ${cont} intentos
+      `);
+    }
+  }
+  while(cont>1);
+
+  return `Lo siento, has perdido!! La palabra secreta era "${secretWord}"`;
+}
+
+console.log(hangmanGame("ramon"));
